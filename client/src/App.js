f9608f237614ff99fc6216ba14a7380d14476ac9
Madmurt <mccartm6@gmail.com>
2021-01-19 18:46:31 +0000

    Authenticating user functionality implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 import './App.css';
 //redux.
 import { Provider } from 'react-redux';
 import store from './store';
 
+if (localStorage.getItem('token')) {
+	setAuthToken(localStorage.getItem('token'));
+}
+
 const App = () => {
+	useEffect(() => {
+		store.dispatch(loadUser());
+	}, []);
+
 	return (
 		<Provider store={store}>
 			<Router>
