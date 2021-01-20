387484eb625591894fa750c1c2f91f266d385252
Madmurt <mccartm6@gmail.com>
2021-01-20 18:51:56 +0000

    Created protected routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import { loadUser } from './actions/auth';
@@ -36,6 +38,11 @@ const App = () => {
 								component={Register}
 							/>
 							<Route exact path='/login' component={Login} />
+							<PrivateRoute
+								exact
+								path='/dashboard'
+								component={Dashboard}
+							/>
 						</Switch>
 					</section>
 				</Fragment>
