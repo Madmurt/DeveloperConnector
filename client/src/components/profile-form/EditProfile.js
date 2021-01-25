5021e13f04b3d033ff5e9536c5a630b5e93bd322
Madmurt <mccartm6@gmail.com>
2021-01-25 19:34:06 +0000

    Edit profle functionality

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -41,11 +41,26 @@ const EditProfile = ({
 					? ''
 					: profile.githubusername,
 			bio: loading || !profile.bio ? '' : profile.bio,
-			twitter: loading || !profile.twitter ? '' : profile.twitter,
-			facebook: loading || !profile.facebook ? '' : profile.facebook,
-			linkedin: loading || !profile.linkedin ? '' : profile.linkedin,
-			youtube: loading || !profile.youtube ? '' : profile.youtube,
-			instagram: loading || !profile.instagram ? '' : profile.instagram,
+			twitter:
+				loading || !profile.social.twitter
+					? ''
+					: profile.social.twitter,
+			facebook:
+				loading || !profile.social.facebook
+					? ''
+					: profile.social.facebook,
+			linkedin:
+				loading || !profile.social.linkedin
+					? ''
+					: profile.social.linkedin,
+			youtube:
+				loading || !profile.social.youtube
+					? ''
+					: profile.social.youtube,
+			instagram:
+				loading || !profile.social.instagram
+					? ''
+					: profile.social.instagram,
 		});
 	}, [loading]);
 
@@ -258,9 +273,9 @@ const EditProfile = ({
 				)}
 
 				<input type='submit' className='btn btn-primary my-1' />
-				<a className='btn btn-light my-1' href='dashboard.html'>
+				<Link className='btn btn-light my-1' to='/dashboard'>
 					Go Back
-				</a>
+				</Link>
 			</form>
 		</Fragment>
 	);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -72,7 +72,9 @@ router.post(
 		if (status) profileFields.status = status;
 		if (githubusername) profileFields.githubusername = githubusername;
 		if (skills) {
-			profileFields.skills = skills.split(',').map((x) => x.trim());
+			profileFields.skills = Array.isArray(skills)
+				? skills
+				: skills.split(',').map((x) => ' ' + x.trim());
 		}
 
 		profileFields.social = {};
